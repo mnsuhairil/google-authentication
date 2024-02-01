87e03d879de943851a5427be58f1aff534b92b73
mnsuhairil <94582522+mnsuhairil@users.noreply.github.com>
2024-02-01 14:26:21 +0800

    Update GoogleAuthController.php

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -1,23 +1,3 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
-
-use App\Http\Controllers\Controller;
-use Laravel\Socialite\Facades\Socialite;
-
-class GoogleAuthController extends Controller
-{
-    public function redirectToGoogle()
-    {
-        return Socialite::driver('google')->redirect();
-    }
-
-    public function handleGoogleCallback()
-    {
-        $user = Socialite::driver('google')->user();
-
-        // Use $user to create or log in the user as needed
-
-        return view('profile', ['user' => $user]);
-    }
-}
+none
